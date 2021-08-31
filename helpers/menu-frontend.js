7123f1892b4da54d6f8955ae8f9c57b72e0ff0e8
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2021-08-31 00:42:52 -0500

    cambiando el menu front

--- helpers/menu-frontend.js
+++ helpers/menu-frontend.js
@@ -1,37 +1,35 @@
+const getMenuFrontEnd = (role = "USER_ROLE") => {
+  const menu = [
+    // {
+    //   titulo: 'Dashboard',
+    //   icono: 'mdi mdi-gauge',
+    //   submenu: [
+    //     { titulo: 'Main', url: '/' },
+    //     { titulo: 'Gráficas', url: 'grafica1' },
+    //     { titulo: 'rxjs', url: 'rxjs' },
+    //     { titulo: 'Promesas', url: 'promesas' },
+    //     { titulo: 'ProgressBar', url: 'progress' },
+    //   ]
+    // },
 
-const getMenuFrontEnd = (role = 'USER_ROLE') => {
+    {
+      titulo: "Mantenimientos",
+      icono: "mdi mdi-folder-lock-open",
+      submenu: [
+        // { titulo: 'Usuarios', url: 'usuarios' },
+        { titulo: "Clinicas", url: "clinicas" },
+        { titulo: "Médicos", url: "medicos" },
+      ],
+    },
+  ];
 
-    const menu = [
-        {
-          titulo: 'Dashboard',
-          icono: 'mdi mdi-gauge',
-          submenu: [
-            { titulo: 'Main', url: '/' },
-            { titulo: 'Gráficas', url: 'grafica1' },
-            { titulo: 'rxjs', url: 'rxjs' },
-            { titulo: 'Promesas', url: 'promesas' },
-            { titulo: 'ProgressBar', url: 'progress' },
-          ]
-        },
-    
-        {
-          titulo: 'Mantenimientos',
-          icono: 'mdi mdi-folder-lock-open',
-          submenu: [
-            // { titulo: 'Usuarios', url: 'usuarios' },
-            { titulo: 'Hospitales', url: 'hospitales' },
-            { titulo: 'Médicos', url: 'medicos' },
-          ]
-        },
-      ];
+  if (role === "ADMIN_ROLE") {
+    menu[1].submenu.unshift({ titulo: "Usuarios", url: "usuarios" });
+  }
 
-    if ( role === 'ADMIN_ROLE' ) {
-        menu[1].submenu.unshift({ titulo: 'Usuarios', url: 'usuarios' })
-    }
-
-    return menu;
-}
+  return menu;
+};
 
 module.exports = {
-    getMenuFrontEnd
-}
+  getMenuFrontEnd,
+};
